9025e1ffca9a7541aee9891a41cb5459887641a7
jww52 <jawszone@gmail.com>
2017-07-15 13:42:38 -0800

    more features

--- js/app.js
+++ js/app.js
@@ -69,17 +69,89 @@ selectedDesign.onchange = function(){
 }
 
 // ”Register for Activities” section of the form:
-// Some events are at the same time as others. If the user selects a workshop, don't allow selection of a workshop at the same date and time -- you should disable the checkbox and visually indicate that the workshop in the competing time slot isn't available.
+
 const activities = document.querySelector('.activities');
+const checkBoxes = activities.querySelectorAll("[type='checkbox']");
 
 
-activities.onchange = function() {
-  const checkBoxes = activities.querySelectorAll("[type='checkbox']");
-    if (checkBoxes[1].checked){
-      checkBoxes[3].disabled = true;
+// const incompatibles
+  const tuesMorning = [1,3],
+        tuesAfternoon = [2,4];
+
+  function disabledStyle(el){
+    el.style.color = "#d3d3d3";
+    el.style.fontStyle = "italic";
+  }
+
+  function undisabledStyle(el){
+    el.style.color = "#000";
+    el.style.fontStyle = "normal";
+  }
+// Some events are at the same time as others. If the user selects a workshop, don't allow selection of a workshop at the same date and time -- you should disable the checkbox and visually indicate that the workshop in the competing time slot isn't available.
+// When a user unchecks an activity, make sure that competing activities (if there are any) are no longer disabled.
+function disableIncompatible (arr) {
+    if(checkBoxes[arr[0]].checked) {
+      checkBoxes[arr[1]].disabled = true;
+      const label = checkBoxes[arr[1]].parentNode;
+      disabledStyle(label);
+    } else {
+      checkBoxes[arr[1]].disabled = false;
+      const label = checkBoxes[arr[1]].parentNode;
+      undisabledStyle(label);
+    }
+    if(checkBoxes[arr[1]].checked) {
+      checkBoxes[arr[0]].disabled = true;
+      const label = checkBoxes[arr[0]].parentNode;
+      disabledStyle(label);
+    } else {
+      checkBoxes[arr[0]].disabled = false;
+      const label = checkBoxes[arr[0]].parentNode;
+      undisabledStyle(label);
     }
 }
 
+function clearOldTotal() {
+  activities.removeChild(total)
+}
 
-// When a user unchecks an activity, make sure that competing activities (if there are any) are no longer disabled.
+function getSum() {
+  let dollars = [];
+
+  let sum = activities.querySelectorAll('input[type="checkbox"]:checked');
+  console.log(sum);
+  for (i =0; i < sum.length; i++) {
+    let activity = sum[i].parentNode.innerHTML;
+    console.log(activity);
+      dollars.push(parseInt( activity.slice(-3) ));
+  }
+  function addUp(total, num) {
+    return total + num;
+  }
+
+  let sumUp = "Total: $" + dollars.reduce(addUp, 0).toString();
+  console.log(dollars);
+  console.log(sumUp);
+  return sumUp;
+}
+
+function removeOldTotal(){
+  console.log(activities.childNodes.length);
+  if(activities.childNodes.length > 17) {
+    let lastNode = activities.lastChild;
+    activities.removeChild(lastNode);
+  }
+}
 // As a user selects activities, a running total should display below the list of checkboxes. For example, if the user selects "Main Conference", then Total: $200 should appear. If they add 1 workshop, the total should change to Total: $300.
+function showTotal(){
+  removeOldTotal();
+  let totalText = getSum();
+  const total = document.createElement("h3");
+  total.innerHTML = totalText;
+  activities.appendChild(total);
+}
+
+activities.onchange = function() {
+  disableIncompatible(tuesMorning);
+  disableIncompatible(tuesAfternoon);
+  showTotal();
+}
